9cabf5a1c63ee9c645a501cd7a00a4f759420f64
Matt Fenwick <mfenwick100@gmail.com>
2014-03-21 13:08:04 -0400

    rename modules, add in module for full validation (parsing + treechecking)

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 'use strict';
 
 
+
 module.exports = {
-    'parser'   : require('./lib/json.js'),
-    'validator': require('./lib/jsontree.js')
+    'parser'     : require('./lib/parser.js')     ,
+    'treechecker': require('./lib/treechecker.js'),
+    'validator'  : require('./lib/validator.js')
 };
 
